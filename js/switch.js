50911ab91e19652ce93e3b7a7f915fda67859bab
fabianfv <fabianfloresvadell@gmail.com>
2020-05-15 23:57:57 +0200

    copiado el codigo a switch.js y refactorizado para que use switch

---
+++ js/switch.js
@@ -0,0 +1,79 @@
+const PIEDRA = 0
+const PAPEL = 1
+const TIJERA = 2
+
+const JUGADOR = 0
+const ORDENADOR = 1
+const EMPATE = 2
+
+function quien_gana(jugador, ordenador) {
+  const OPCIONES = ["PIEDRA", "PAPEL", "TIJERA"]
+  const JUGADA = OPCIONES[jugador] + " vs " + OPCIONES[ordenador]
+
+  switch (JUGADA) {
+    case "PIEDRA vs PIEDRA":
+    case "TIJERA vs TIJERA":
+    case "PAPEL vs PAPEL":
+      return EMPATE
+    case "PIEDRA vs TIJERA":
+    case "PAPEL vs PIEDRA":
+    case "TIJERA vs PAPEL":
+      return JUGADOR
+    default:
+      return ORDENADOR
+  }
+}
+
+function crear_mensaje(eleccion_jugador, eleccion_ordenador, ganador) {
+  const OPCIONES = ["PIEDRA", "PAPEL", "TIJERA"]
+
+  switch (ganador) {
+    case EMPATE:
+      return `¡¡¡EMPATE!!! Ambos jugadores eligieron: ${OPCIONES[eleccion_jugador]}`
+    case JUGADOR:
+      return `GANA JUGADOR, porque ${OPCIONES[eleccion_jugador]} vence a ${OPCIONES[eleccion_ordenador]}`
+    case ORDENADOR:
+      return `GANA ORDENADOR, porque ${OPCIONES[eleccion_ordenador]} vence a ${OPCIONES[eleccion_jugador]}`
+  }
+}
+
+function opcion_al_azar() {
+  const MAX = 2
+  const MIN = 0
+  return Math.round(Math.random() * (MAX - MIN) + MIN)
+}
+
+function informar_ganador(mensaje) {
+  console.log(mensaje)
+}
+
+function despedirse() {
+  console.clear()
+  console.log("Gracias por jugar a PIEDRA PAPEL TIJERA. Vuelve pronto.")
+}
+
+function jugar() {
+  let eleccion_jugador, eleccion_ordenador, ganador, mensaje
+  const MENU =
+    "Elige una opción\n\n1- PIEDRA\n2- PAPEL\n3- TIJERA\n\nPulsa la tecla ESC o el botón Cancelar para terminar\n\n"
+
+  while (true) {
+    eleccion_jugador = prompt(MENU)
+
+    if (!eleccion_jugador) break
+
+    eleccion_jugador = Number(eleccion_jugador)
+
+    if (![PIEDRA, PAPEL, TIJERA].includes(--eleccion_jugador)) continue
+
+    eleccion_ordenador = opcion_al_azar()
+
+    ganador = quien_gana(eleccion_jugador, eleccion_ordenador)
+    mensaje = crear_mensaje(eleccion_jugador, eleccion_ordenador, ganador)
+    informar_ganador(mensaje)
+  }
+
+  despedirse()
+}
+
+jugar()
