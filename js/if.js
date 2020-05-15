4add6033c4f90b053fe73612dbec1f2bfae22f87
fabianfv <fabianfloresvadell@gmail.com>
2020-05-15 23:47:44 +0200

    commit inicial: constantes y armazones de funciones - if.js

---
+++ js/if.js
@@ -0,0 +1,21 @@
+const PIEDRA = 0
+const PAPEL = 1
+const TIJERA = 2
+
+const JUGADOR = 0
+const ORDENADOR = 1
+const EMPATE = 2
+
+function quien_gana() {}
+
+function crear_mensaje() {}
+
+function opcion_al_azar() {}
+
+function informar_ganador() {}
+
+function despedirse() {}
+
+function jugar() {}
+
+jugar()
